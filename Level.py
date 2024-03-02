76a23d7bd502838504032922edf59ce96c31ee90
Jonas Send <jonas@jonassend.com>
2024-03-02 16:41:11 +0100

    improve level and include title

--- Level.py
+++ Level.py
@@ -4,6 +4,11 @@ from Block import Block
 from Constants import WINDOW_WIDTH, WINDOW_HEIGHT
 from Goal import Goal
 
+BLOCK_WIDTH = 75
+BORDER_WIDTH = 32
+GOAL_WIDTH = 100
+GOAL_HEIGHT = 16
+
 
 class Level:
     def __init__(self):
@@ -15,21 +20,24 @@ class Level:
         # Example level layout
         level_layout = [
             # Ground
-            (0, WINDOW_HEIGHT - 32, WINDOW_WIDTH, 32),
+            (0, WINDOW_HEIGHT - BORDER_WIDTH, WINDOW_WIDTH, BORDER_WIDTH),
             # Ceiling
-            (0, 0, WINDOW_WIDTH, 32),
+            (0, 0, WINDOW_WIDTH, BORDER_WIDTH),
             # Left wall
-            (0, 0, 32, WINDOW_HEIGHT),
+            (0, 0, BORDER_WIDTH, WINDOW_HEIGHT),
             # Right wall
-            (WINDOW_WIDTH - 32, 0, 32, WINDOW_HEIGHT),
-            # Obstsacle
-            (400, WINDOW_HEIGHT - 250, 100, 300),
+            (WINDOW_WIDTH - BORDER_WIDTH, 0, BORDER_WIDTH, WINDOW_HEIGHT),
+            # Obstsacles
+            (250, WINDOW_HEIGHT - 100 - BORDER_WIDTH, BLOCK_WIDTH, 100),
+            (250 + BLOCK_WIDTH, WINDOW_HEIGHT - 200 - BORDER_WIDTH, BLOCK_WIDTH, 200),
+            (250 + 2 * BLOCK_WIDTH, WINDOW_HEIGHT - 420 - BORDER_WIDTH, BLOCK_WIDTH, 420),
+            (650, WINDOW_HEIGHT - 220 - BORDER_WIDTH, BLOCK_WIDTH, 220)
         ]
         
         for block in level_layout:
             self.blocks.add(Block(*block))
             
-        self.goal = Goal(600, WINDOW_HEIGHT - 64, 32, 32)
+        self.goal = Goal(WINDOW_WIDTH - GOAL_WIDTH - BORDER_WIDTH, WINDOW_HEIGHT - BORDER_WIDTH - GOAL_HEIGHT, GOAL_WIDTH, GOAL_HEIGHT)
 
     def draw(self, screen):
         for block in self.blocks:
--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import pygame
 import sys
 
 # Initialize Pygame
-from Constants import BLACK, WHITE, WINDOW_WIDTH, WINDOW_HEIGHT
+from Constants import BLACK, WHITE, WINDOW_WIDTH, WINDOW_HEIGHT, VIOLET
 from pygame.locals import RESIZABLE, DOUBLEBUF, HWSURFACE
 from Level import Level
 from PastPlayer import PastPlayer
 from Player import Player
 from util.timer import format_time
 
+GAME_NAME = "TIME JUMPER"
 
 pygame.init()
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), RESIZABLE | DOUBLEBUF | HWSURFACE)
@@ -16,8 +17,9 @@ WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), RESIZABLE | DOUB
 SCREEN = WINDOW.copy()
 CLOCK = pygame.time.Clock()
 timer_font = pygame.font.Font(None, 36)
-font_level_complete = pygame.font.Font(None, 72)
-pygame.display.set_caption("TIME JUMPER")
+name_font = pygame.font.Font(None, 54)
+font_level_complete = pygame.font.Font(None, 96)
+pygame.display.set_caption(GAME_NAME)
 
 level = Level()
 
@@ -81,13 +83,16 @@ while running:
 
     # Format and render the time
     timer_text = format_time(elapsed_seconds)
-    text_surface = timer_font.render(timer_text, True, WHITE)
+    text_timer_surface = timer_font.render(timer_text, True, WHITE)
+    text_name_surface = name_font.render(GAME_NAME, True, WHITE)
 
     # Drawing
     SCREEN.fill(BLACK)
     level.draw(SCREEN)
-    text_rect = text_surface.get_rect(topright=(WINDOW_WIDTH - 64, 64))
-    SCREEN.blit(text_surface, text_rect)
+    text_timer_rect = text_timer_surface.get_rect(topright=(WINDOW_WIDTH - 64, 64))
+    text_name_rect = text_name_surface.get_rect(topleft=(64, 64))
+    SCREEN.blit(text_timer_surface, text_timer_rect)
+    SCREEN.blit(text_name_surface, text_name_rect)
     SCREEN.blit(player.surf, player.rect)
 
     for pp in past_players:
@@ -97,10 +102,10 @@ while running:
     # Display the message if level is complete
     if level.complete:
         # Render the text to a surface
-        text_surface = font_level_complete.render('Level Complete!', True, WHITE)  # White text
+        text_timer_surface = font_level_complete.render('Level Complete!', True, VIOLET)  # White text
         # Position the text in the center of the SCREEN
-        text_rect = text_surface.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-        SCREEN.blit(text_surface, text_rect)
+        text_timer_rect = text_timer_surface.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 3))
+        SCREEN.blit(text_timer_surface, text_timer_rect)
 
     WINDOW.blit(pygame.transform.scale(SCREEN, WINDOW.get_rect().size), (0, 0))
     pygame.display.flip()
